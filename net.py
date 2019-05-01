773d8a54f84a10a9a006adf5493addfbfc674a60
karoly-hars <harsanyika@gmail.com>
2019-05-01 22:26:35 +0200

    code cleanup

--- net.py
+++ net.py
@@ -2,11 +2,10 @@ import torch.nn as nn
 import torch
 import os
 
-def conv3x3(in_planes, out_planes, stride=1):
-    "3x3 convolution with padding"
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                     padding=1, bias=False)
 
+def conv3x3(in_planes, out_planes, stride=1):
+    # 3x3 convolution with padding
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
 
 
 class Bottleneck(nn.Module):
@@ -45,69 +44,65 @@ class Bottleneck(nn.Module):
         out = self.relu(out)
 
         return out
-      
 
 
 def get_incoming_shape(incoming):
-    """ Returns the incoming data shape """
     size = incoming.size()
     return [size[0], size[1], size[2], size[3]]
+
 
 def interleave(tensors, axis):
     # change the first element (batch_size to -1)
     old_shape = get_incoming_shape(tensors[0])[1:]
     new_shape = [-1] + old_shape
-    
+
     # double 1 dimension
     new_shape[axis] *= len(tensors)
-    
-    # pack the tensors on top of each other 
-    stacked = torch.stack(tensors, axis+1)
-    
+
+    # pack the tensors on top of each other
+    stacked = torch.stack(tensors, axis + 1)
+
     # reshape and return
     reshaped = stacked.view(new_shape)
     return reshaped
 
 
-        
-class UnPool_as_Conv(nn.Module):       
+class UnpoolingAsConvolution(nn.Module):
     def __init__(self, inplanes, planes):
-        super(UnPool_as_Conv, self).__init__()
-        
+        super(UnpoolingAsConvolution, self).__init__()
+
         # interleaving convolutions
-        self.conv_A = nn.Conv2d(in_channels = inplanes, out_channels = planes, kernel_size = (3,3), stride = 1, padding = 1)
-        self.conv_B = nn.Conv2d(in_channels = inplanes, out_channels = planes, kernel_size = (2,3), stride = 1, padding = 0)
-        self.conv_C = nn.Conv2d(in_channels = inplanes, out_channels = planes, kernel_size = (3,2), stride = 1, padding = 0)
-        self.conv_D = nn.Conv2d(in_channels = inplanes, out_channels = planes, kernel_size = (2,2), stride = 1, padding = 0)
-        
-        
-    def forward(self,x):
-        output_A = self.conv_A(x)
-        
-        padded_B = nn.functional.pad(x, (1,1,0,1))
-        output_B = self.conv_B(padded_B)
-        
-        padded_C = nn.functional.pad(x, (0,1,1,1))
-        output_C = self.conv_C(padded_C)
-        
-        padded_D = nn.functional.pad(x, (0,1,0,1))
-        output_D = self.conv_D(padded_D)       
-        
-        left = interleave([output_A, output_B], axis = 2)
-        right = interleave([output_C, output_D], axis = 2)
-        Y = interleave([left, right], axis = 3)
-                
+        self.conv_A = nn.Conv2d(in_channels=inplanes, out_channels=planes, kernel_size=(3, 3), stride=1, padding=1)
+        self.conv_B = nn.Conv2d(in_channels=inplanes, out_channels=planes, kernel_size=(2, 3), stride=1, padding=0)
+        self.conv_C = nn.Conv2d(in_channels=inplanes, out_channels=planes, kernel_size=(3, 2), stride=1, padding=0)
+        self.conv_D = nn.Conv2d(in_channels=inplanes, out_channels=planes, kernel_size=(2, 2), stride=1, padding=0)
+
+    def forward(self, x):
+        output_a = self.conv_A(x)
+
+        padded_b = nn.functional.pad(x, (1, 1, 0, 1))
+        output_b = self.conv_B(padded_b)
+
+        padded_c = nn.functional.pad(x, (0, 1, 1, 1))
+        output_c = self.conv_C(padded_c)
+
+        padded_d = nn.functional.pad(x, (0, 1, 0, 1))
+        output_d = self.conv_D(padded_d)
+
+        left = interleave([output_a, output_b], axis=2)
+        right = interleave([output_c, output_d], axis=2)
+        Y = interleave([left, right], axis=3)
+
         return Y
 
 
-
-class UpProj(nn.Module):  
+class UpProjection(nn.Module):
     def __init__(self, inplanes, planes):
-        super(UpProj, self).__init__()        
-        
-        self.unpool_main = UnPool_as_Conv(inplanes, planes)
-        self.unpool_res = UnPool_as_Conv(inplanes, planes)
-             
+        super(UpProjection, self).__init__()
+
+        self.unpool_main = UnpoolingAsConvolution(inplanes, planes)
+        self.unpool_res = UnpoolingAsConvolution(inplanes, planes)
+
         self.main_branch = nn.Sequential(
             self.unpool_main,
             nn.BatchNorm2d(planes),
@@ -115,81 +110,77 @@ class UpProj(nn.Module):
             nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(planes)
         )
-        
+
         self.residual_branch = nn.Sequential(
             self.unpool_res,
             nn.BatchNorm2d(planes),
         )
 
-        self.relu = nn.ReLU(inplace=False)       
-        
-        
+        self.relu = nn.ReLU(inplace=False)
+
     def forward(self, input_data):
-        
         x = self.main_branch(input_data)
-        res = self.residual_branch(input_data)             
-        x += res  
+        res = self.residual_branch(input_data)
+        x += res
         x = self.relu(x)
-        
+
         return x
+
 
 class ConConv(nn.Module):
     def __init__(self, inplanes_x1, inplanes_x2, planes):
-        super(ConConv, self).__init__()        
+        super(ConConv, self).__init__()
         self.conv = nn.Conv2d(inplanes_x1 + inplanes_x2, planes, kernel_size=1, bias=True)
-    
+
     def forward(self, x1, x2):
         x1 = torch.cat([x2, x1], dim=1)
         x1 = self.conv(x1)
         return x1
 
 
+class ResnetUnetHybrid(nn.Module):
 
-class ResNetUpProjUnetHyb(nn.Module):
- 
-    def __init__(self, block, layers):
-        
+    def __init__(self, block, layers, output_channels):
+
         self.inplanes = 64
-        
-        super(ResNetUpProjUnetHyb, self).__init__()
+
+        super(ResnetUnetHybrid, self).__init__()
         self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        
+
         self.layer1 = self._make_layer(block, 64, layers[0])
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
         self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
         self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
-               
-        
-        ''' additional up projection layers parts '''
+
+        # additional up projection layers parts
         self.conv2 = nn.Conv2d(2048, 1024, 1, bias=True)
         self.bn2 = nn.BatchNorm2d(1024)
-        
-        self.up_proj1 = UpProj(1024, 512)
-        self.up_proj2 = UpProj(512, 256)
-        self.up_proj3 = UpProj(256, 128)
-        self.up_proj4 = UpProj(128, 64)
-        
+
+        self.up_proj1 = UpProjection(1024, 512)
+        self.up_proj2 = UpProjection(512, 256)
+        self.up_proj3 = UpProjection(256, 128)
+        self.up_proj4 = UpProjection(128, 64)
+
         self.drop = nn.Dropout(0.5, False)
-        self.conv3 = nn.Conv2d(64, 1, kernel_size=3, stride=1, padding=1, bias=True)
-        
-        ''' padding + concat for unet stuff '''
+        self.conv3 = nn.Conv2d(64, output_channels, kernel_size=3, stride=1, padding=1, bias=True)
+
+        # padding + concat for unet stuff
         self.con_conv1 = ConConv(1024, 512, 512)
-        self.con_conv2 = ConConv(512, 256, 256) 
-        self.con_conv3 = ConConv(256, 128, 128) 
-        self.con_conv4 = ConConv(64, 64, 64) 
-        
-        
+        self.con_conv2 = ConConv(512, 256, 256)
+        self.con_conv3 = ConConv(256, 128, 128)
+        self.con_conv4 = ConConv(64, 64, 64)
+
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 nn.init.normal_(m.weight, 0, 0.01)
-                                
+
             elif isinstance(m, nn.BatchNorm2d):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
-                
+
     def _make_layer(self, block, planes, blocks, stride=1):
         downsample = None
         if stride != 1 or self.inplanes != planes * block.expansion:
@@ -199,62 +190,72 @@ class ResNetUpProjUnetHyb(nn.Module):
                 nn.BatchNorm2d(planes * block.expansion),
             )
 
-        layers = []
+        layers = list()
         layers.append(block(self.inplanes, planes, stride, downsample))
         self.inplanes = planes * block.expansion
         for i in range(1, blocks):
             layers.append(block(self.inplanes, planes))
 
         return nn.Sequential(*layers)
-        
+
     def forward(self, x):
         x = self.conv1(x)
         x = self.bn1(x)
         x_to_conv4 = self.relu(x)
-        
+
         x = self.maxpool(x_to_conv4)
         x_to_conv3 = self.layer1(x)
         x_to_conv2 = self.layer2(x_to_conv3)
         x_to_conv1 = self.layer3(x_to_conv2)
         x = self.layer4(x_to_conv1)
-          
-        ''' additional layers '''
+
+        # additional layers
         x = self.conv2(x)
         x = self.bn2(x)
-        
-        ''' up project part'''
+
+        # up project part
         x = self.up_proj1(x)
         x = self.con_conv1(x, x_to_conv1)
-        
+
         x = self.up_proj2(x)
         x = self.con_conv2(x, x_to_conv2)
-        
+
         x = self.up_proj3(x)
         x = self.con_conv3(x, x_to_conv3)
-        
+
         x = self.up_proj4(x)
         x = self.con_conv4(x, x_to_conv4)
-        
+
         x = self.drop(x)
         x = self.conv3(x)
         x = self.relu(x)
-        
+
         return x
 
 
-def hyb_net(load_path='resnet_hyb_DE.model', use_gpu=False, **kwargs):
+def get_model(output, use_gpu=False):
+    if output == "depth":
+        load_path = "resnet_hyb_DE.model"
+        model = ResnetUnetHybrid(Bottleneck, [3, 4, 6, 3], 1)
 
-    model = ResNetUpProjUnetHyb(Bottleneck, [3, 4, 6, 3], **kwargs)
-    
-    # download the weight in case they are not present
-    if not os.path.exists(load_path):
-        print('Downloading model weights...')
-        os.system("wget https://www.dropbox.com/s/on236kfn6v8swl9/resnet_hyb_DE.model")
-        print('Done.')
-            
+        # download the weight in case they are not present
+        if not os.path.exists(load_path):
+            print("Downloading model weights...")
+            os.system("wget https://www.dropbox.com/s/on236kfn6v8swl9/resnet_hyb_DE.model")
+            print("Done.")
+    else:
+        load_path = "resnet_hyb_seg.model"
+        model = ResnetUnetHybrid(Bottleneck, [3, 4, 6, 3], 7)
+
+        # download the weight in case they are not present
+        if not os.path.exists(load_path):
+            print("Downloading model weights...")
+            os.system("wget https://www.dropbox.com/s/f4hw5p4wawx9u1z/resnet_hyb_seg.model")
+            print("Done.")
+
     if use_gpu:
         model.load_state_dict(torch.load(load_path))
     else:
-        model.load_state_dict(torch.load(load_path, map_location='cpu'))
+        model.load_state_dict(torch.load(load_path, map_location="cpu"))
 
     return model
--- predict_img.py
+++ predict_img.py
@@ -2,37 +2,34 @@ import torch
 from torch.autograd import Variable
 from utils import load_img, show_img_preds, show_point_cloud
 import argparse
-import net_DE
-import net_seg
+import net
 import torch.nn.functional as F
-
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('img_path', type=str,  help='path to the RGB image input')
-    parser.add_argument('focal_len', type=float,  help='focal length of the camera')
+    parser.add_argument("img_path", type=str,  help="path to the RGB image input")
+    parser.add_argument("focal_len", type=float,  help="focal length of the camera")
     args = parser.parse_args()
     
     # switching to GPU if possible
     use_gpu = torch.cuda.is_available()
-    print('\nusing GPU:', use_gpu)    
-    
-    
-    # loading models
-    print('\nLoading models...')
-    model_DE = net_DE.hyb_net(use_gpu=use_gpu)
-    if use_gpu:
-        model_DE = model_DE.cuda()
+    print("\nusing GPU:", use_gpu)    
 
-    model_seg = net_seg.hyb_net(use_gpu=use_gpu)
+    # loading models
+    print("\nLoading models...")
+    model_de = net.get_model(output="depth", use_gpu=use_gpu)
+    if use_gpu:
+        model_de = model_de.cuda()
+
+    model_seg = net.get_model(output="seg", use_gpu=use_gpu)
     if use_gpu:
         model_seg = model_seg.cuda()
             
-    # setting models to evalutation mode
-    model_DE.eval()
+    # setting models to evaluation mode
+    model_de.eval()
     model_seg.eval()
-    print('Done.')
+    print("Done.")
       
     # reading image
     img = load_img(args.img_path)
@@ -42,30 +39,27 @@ def main():
         img = Variable(img.cuda())
     else:
         img = Variable(img)
-    print('Plotting...')
-    output_de = model_DE(img)
+    print("Plotting...")
+    output_de = model_de(img)
     output_seg = model_seg(img)
     
-    # bilinear upsampling   
-    resizer = torch.nn.Upsample(size=(320,320), mode='bilinear', align_corners=True)
-    output_seg = resizer(output_seg)
+    # bilinear upsampling
+    output_de = F.interpolate(output_de, size=(320, 320), mode="bilinear", align_corners=True)
+    output_seg = F.interpolate(output_seg, size=(320, 320), mode="bilinear", align_corners=True)
+
+    # softmax for semantic segmentation
     output_seg = F.softmax(output_seg, dim=1)
-    output_de = resizer(output_de)
-    
-        
-    # ploting the results
+
+    # plotting the results
     output_de = output_de.cpu()[0].data.numpy()
-    output_seg= output_seg.cpu()[0].data.numpy()
+    output_seg = output_seg.cpu()[0].data.numpy()
     img = img.cpu()[0].data.numpy()  
     show_img_preds(img, output_de, output_seg, uc_th=0.9, apply_depth_mask=True)
-    
-    
-    # visualize the point cloud
+
+    # visualize the points in 3D
     show_point_cloud(img, output_de, output_seg, args.focal_len, uc_th=0.9, apply_depth_mask=True)
-    print('Done')
+    print("Done")
     
 
 if __name__ == "__main__":
     main()
-
-
